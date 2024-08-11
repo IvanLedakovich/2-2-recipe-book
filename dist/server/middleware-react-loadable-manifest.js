b9ad92855199ee38f613d2b2037beae8761595f2
HUBSlover <ivanled2002@gmail.com>
2024-08-11 23:13:18 +0200

    cleaned-up

--- dist/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app\\\\[[...slug]]\\\\client.tsx -> ../../App\":{\"id\":\"app\\\\[[...slug]]\\\\client.tsx -> ../../App\",\"files\":[]}}"
--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"MMASx0x4VSah/mdyWq6x5oS6LYSwVCZ+v2ifxSYMjNs=\"\n}"
